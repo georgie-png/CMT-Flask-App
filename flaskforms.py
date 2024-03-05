72cb8928271b4fdda5fb5a9db0692d1b508d56ae
Georgie <simmsgeorge7@gmail.com>
2024-03-05 15:44:51 +0000

    added a save data function

--- flaskforms.py
+++ flaskforms.py
@@ -4,45 +4,67 @@ import json
 
 app = Flask(__name__)
 
-def loadJSON():
+data = None
+
+def loadDataJSON():
     #Load data from a JSON file 
-    with open("CMT_Data_Lables.json","r") as read_file:
-        data=json.load(read_file)
-     #returns the value associated with the key "food_labels" from the loaded JSON data
-        return data["food_labels"]
+    with open("CMT_Data.json","r") as read_file:
+        global data
+        data = json.load(read_file)
+
+def saveDataJSON(values):
+    
+    global data
+
+    try:
+        food_data = data["food_data"]
+        food_data.append(values)
+        data["food_data"] = food_data
+
+    except:
+        data["food_data"] = [values]
+
+    #Load data from a JSON file 
+    with open("CMT_Data.json","w") as write_file:
+        json.dump(data, write_file, indent=4)
+
+    
 
 # Renders the basic template as the index
 @app.route("/")
 def index():
+    
+    loadDataJSON()
     return render_template("template.html")
 
 # Renders the pickup form from dynamic values
 @app.route("/pickup" ,methods=['GET', 'POST'])
 def pickupform():
-    # Dynamic data to be passed to the template which is food_labels
-    dynamic_labels =  loadJSON()
 
     if request.method == 'POST':
         # Print the form data to the console
         for key, value in request.form.items():
             print(f'{key}: {value}')
 
-    
-    return render_template("pickupform.html", dynamic_labels=dynamic_labels)
+    global data
+
+    return render_template("pickupform.html", dynamic_labels=data["food_labels"])
 
 # Renders the pickup form from dynamic values
 @app.route("/kitchen",methods=['GET', 'POST'])
 def kitchenform():
-        # Dynamic data to be passed to the template which is food_labels
-    dynamic_labels =  loadJSON()
+    global data
 
     if request.method == 'POST':
         # Print the form data to the console
+        values = []
         for key, value in request.form.items():
             print(f'{key}: {value}')
+            values.append(value)            
 
-    
-    return render_template("kitchenform.html", dynamic_labels=dynamic_labels)
+        saveDataJSON(values)
+
+    return render_template("kitchenform.html", dynamic_labels=data["food_labels"])
 
     
 if __name__ == "__main__":
