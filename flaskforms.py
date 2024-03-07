552ce894bd4de3a9515d2d0b42ee77df3d18827e
mkwawama <79860095+mkwawama@users.noreply.github.com>
2024-03-07 17:27:58 +0000

    New Response Page Added

--- flaskforms.py
+++ flaskforms.py
@@ -40,14 +40,19 @@ def index():
 # Renders the pickup form from dynamic values
 @app.route("/pickup" ,methods=['GET', 'POST'])
 def pickupform():
+    global data
 
     if request.method == 'POST':
         # Print the form data to the console
+        values = {}
         for key, value in request.form.items():
             print(f'{key}: {value}')
-
-    global data
-
+            #Values is now dictionary data structure store values as a key-value pair.
+            values[key] =value          
+        saveDataJSON(values)
+        # Render response page template with form data if request method is post 
+        return render_template("responsepage.html", values=values)
+    
     return render_template("pickupform.html", dynamic_labels=data["food_labels"])
 
 # Renders the pickup form from dynamic values
@@ -55,16 +60,17 @@ def pickupform():
 def kitchenform():
     global data
 
-
- 
     if request.method == 'POST':
         # Print the form data to the console
-        values = []
+        values = {}
         for key, value in request.form.items():
             print(f'{key}: {value}')
-            values.append(value)            
-
+            #Values is now dictionary data structure store values as a key-value pair.
+            values[key] =value          
         saveDataJSON(values)
+        # Render response page template with form data if request method is post 
+        return render_template("responsepage.html", values=values)
+    # Render form template with dynamic values
     return render_template("kitchenform.html", dynamic_labels=data["food_labels"],dynamic_options=data['kitchen_labels'])
 
     
