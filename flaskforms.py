c2da324dcfe8f61f95d584eb9fc2711a9b697503
Georgie <simmsgeorge7@gmail.com>
2024-03-27 17:01:17 +0000

    tweaked it for the docker file

--- flaskforms.py
+++ flaskforms.py
@@ -8,128 +8,123 @@ data = CMT_Data("Data/CMT_Data.json")
 Pass = "WeLoveAutonomy"
 admin_password = "admin_pass"
 
-# Function that creates the flask app for us
-def smuc_app(test_config=None):
 
-    # initialise flask app
-    app = Flask(__name__)
+# initialise flask app
+app = Flask(__name__)
 
-    app.secret_key = 'kwer!wh83£$47dh82u&wh28?'
-    print("#############################")
-    print("#############################")
-    print("Password: ", Pass)
-    print("#############################")
-    print("#############################")
+app.secret_key = 'kwer!wh83£$47dh82u&wh28?'
+print("#############################")
+print("#############################")
+print("Password: ", Pass)
+print("#############################")
+print("#############################")
 
-    # Renders the basic template as the index
-    @app.route("/", methods=['GET', 'POST'])
-    def index():
+# Renders the basic template as the index
+@app.route("/", methods=['GET', 'POST'])
+def index():
 
-        # if logged in render  nav page
-        if session.get('logged_ in') and session["logged_ in"]==True:
-            return render_template("Nav.html")
+    # if logged in render  nav page
+    if session.get('logged_ in') and session["logged_ in"]==True:
+        return render_template("Nav.html")
 
-        # on l0gin button
-        if request.method == "POST":
-            # get password login
-            pw = request.form.get("pw") 
-            # if password matches
-            if pw == Pass:
-                # set session as logged in
-                session["logged_ in"]=True
-                # render nav
-                return render_template("Nav.html")            
-            
-            
-        return render_template("index.html")
-
-    # Renders the pickup form from dynamic values
-    ### Curently not in use
-    #@app.route("/pickup" ,methods=['GET', 'POST']) 
-    def pickupform():
-
-        # if not logged in redirect to main page
-        if not session.get('logged_ in') or session["logged_ in"]==True:
-            return redirect("/")
-
-        global data
-
-        if request.method == 'POST':
-            # add form data to the data class and return the display values
-            display_values =  data.add_pickup_data(request.form.items())
-            # Render response page template with form data if request method is post 
-            return render_template("responsepage.html", values=display_values)
+    # on l0gin button
+    if request.method == "POST":
+        # get password login
+        pw = request.form.get("pw") 
+        # if password matches
+        if pw == Pass:
+            # set session as logged in
+            session["logged_ in"]=True
+            # render nav
+            return render_template("Nav.html")            
         
-        return render_template("pickupform.html", dynamic_labels=data.json["food_labels"])
-
-    # Renders the kitchen form from dynamic values
-    @app.route("/kitchen",methods=['GET', 'POST'])
-    def kitchenform():
-
-        # if not logged in redirect to main page
-        if not session.get('logged_ in') or session["logged_ in"]!=True:
-            return redirect("/")
         
-        global data
+    return render_template("index.html")
 
-        if request.method == 'POST':
-            # Add form data to data class and get display data back.
-            display_values =  data.add_kitchen_data(request.form.items())
-            # Render response page template with form data if request method is post 
-            return render_template("responsepage.html", values=display_values)
-        
-        # Render form template with dynamic values
-        return render_template("kitchenform.html", dynamic_labels=data.json["food_labels"],dynamic_options=data.json['kitchen_labels'])
+# Renders the pickup form from dynamic values
+### Curently not in use
+#@app.route("/pickup" ,methods=['GET', 'POST']) 
+def pickupform():
+
+    # if not logged in redirect to main page
+    if not session.get('logged_ in') or session["logged_ in"]==True:
+        return redirect("/")
+
+    global data
+
+    if request.method == 'POST':
+        # add form data to the data class and return the display values
+        display_values =  data.add_pickup_data(request.form.items())
+        # Render response page template with form data if request method is post 
+        return render_template("responsepage.html", values=display_values)
     
+    return render_template("pickupform.html", dynamic_labels=data.json["food_labels"])
 
+# Renders the kitchen form from dynamic values
+@app.route("/kitchen",methods=['GET', 'POST'])
+def kitchenform():
 
-     # Renders the overview page displaying data for all kitchens.
-    @app.route("/overview")
-    def overview():
-
-        # if not logged in redirect to main page
-        if not session.get('logged_ in') or session["logged_ in"]!=True:
-            return redirect("/")
-
-        global data  # Indicates that 'data' is a global variable
-
-        #Get the kitchens array direclty from CMT_Data 
-        kitchens = data.kitchens
-
-        #Intialize an empty list to store display values for all kitchens
-        kitchens_display_values = []
-
-         # Iterate through each kitchen in the kitchens array
-        for kitchen in kitchens:
-        #Get the display value for the current kitchen
-            display_val = kitchen.get_display_val()
-        # Append the display value to the list
-            kitchens_display_values.append(display_val)
-        # Return the list of display values for all kitchens 
-        return render_template("overview.html", kitchens_display_values=kitchens_display_values)
-        
-    # downloads the 
-    @app.route('/download/<admin_pass>')
-    def download_file(admin_pass):
-
-        if admin_pass!= admin_password or not session.get('logged_ in') or session["logged_ in"]!=True:
-            return redirect("/")
-        
-        global data
-
-        path = data.working_file
-
-        return send_file(path, as_attachment=True)
+    # if not logged in redirect to main page
+    if not session.get('logged_ in') or session["logged_ in"]!=True:
+        return redirect("/")
     
+    global data
 
-    # returns the flask app
-    return app
+    if request.method == 'POST':
+        # Add form data to data class and get display data back.
+        display_values =  data.add_kitchen_data(request.form.items())
+        # Render response page template with form data if request method is post 
+        return render_template("responsepage.html", values=display_values)
     
+    # Render form template with dynamic values
+    return render_template("kitchenform.html", dynamic_labels=data.json["food_labels"],dynamic_options=data.json['kitchen_labels'])
+
+
+
+    # Renders the overview page displaying data for all kitchens.
+@app.route("/overview")
+def overview():
+
+    # if not logged in redirect to main page
+    if not session.get('logged_ in') or session["logged_ in"]!=True:
+        return redirect("/")
+
+    global data  # Indicates that 'data' is a global variable
+
+    #Get the kitchens array direclty from CMT_Data 
+    kitchens = data.kitchens
+
+    #Intialize an empty list to store display values for all kitchens
+    kitchens_display_values = []
+
+        # Iterate through each kitchen in the kitchens array
+    for kitchen in kitchens:
+    #Get the display value for the current kitchen
+        display_val = kitchen.get_display_val()
+    # Append the display value to the list
+        kitchens_display_values.append(display_val)
+    # Return the list of display values for all kitchens 
+    return render_template("overview.html", kitchens_display_values=kitchens_display_values)
+    
+# downloads the 
+@app.route('/download/<admin_pass>')
+def download_file(admin_pass):
+
+    if admin_pass!= admin_password or not session.get('logged_ in') or session["logged_ in"]!=True:
+        return redirect("/")
+    
+    global data
+
+    path = data.working_file
+
+    return send_file(path, as_attachment=True)
+
+
 
     
 if __name__ == "__main__":
     # run the smuc_app function and get the app
-    app = smuc_app()
+
     # run it!
     app.run(debug=True)
 
